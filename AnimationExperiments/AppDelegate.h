5cc4161a546d379700fdc9865abec81945420bcd
Ian Dundas <contact@iandundas.co.uk>
2013-09-23 11:58:05 +0700

    Initial Commit

---
+++ AnimationExperiments/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimationExperiments
+//
+//  Created by Ian Dundas on 23/09/2013.
+//  Copyright (c) 2013 Ian Dundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimationExperimentsTests/AnimationExperimentsTests.m
@@ -0,0 +1,34 @@
+//
+//  AnimationExperimentsTests.m
+//  AnimationExperimentsTests
+//
+//  Created by Ian Dundas on 23/09/2013.
+//  Copyright (c) 2013 Ian Dundas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnimationExperimentsTests : XCTestCase
+
+@end
+
+@implementation AnimationExperimentsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
